3cdbe277bc96ed6a62d0e28e08480166d9656b9b
bogdanputinelu <bogdanputinelu@yahoo.com>
2022-11-17 20:47:07 +0200

    curs1

---
+++ curs01/curs.py
@@ -0,0 +1,32 @@
+# variabila1 = 1
+# variabila2 = 2
+# mesaj = "variabila 1 e mai mica"
+# if (mesaj1 := "egalitate") and variabila1 == variabila2 :
+#     mesaj = mesaj1
+# elif variabila1 > variabila2:
+#     mesaj = "variabila 1 e mai mare"
+# # mesaj = "egalitate" if variabila1 == variabila2 else "inegalitate"
+# print(mesaj1)
+
+
+# variabila1 = 1
+# variabila2 = 3
+# while variabila1 < variabila2:
+#     print(variabila2, 'rand 7')
+#     variabila1 +=1
+# else:
+#     print(variabila2, 'rand 10')
+
+# for i,v in enumerate("Ana are mere"):
+#     print(i,v)
+
+# lista = ['cumaraturi', 'citit']
+# for i,v in enumerate(lista):
+#     print(i,v)
+
+# for i in range(3, 10, 3):
+#     print(i)
+
+dictionar = {"carte1": 1, "carte2": 2, "carte3": 3}
+for i, v in dictionar.items():
+    print(i, v)
